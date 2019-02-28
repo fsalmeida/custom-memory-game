88a6ccb9ec913248bf3a2c79b20e716e3c74baac
Felipe <felipe.silvalmeida@gmail.com>
2019-02-27 22:16:25 -0300

    Melhoria no tamanho da fonte

--- src/features/game/card.js
+++ src/features/game/card.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { isMobile } from "react-device-detect";
+
+const minFontSize = isMobile ? 8 : 16;
+const maxFontSize = isMobile ? 20 : 30;
+const maxWordSize = 15;
+const minWordSize = 2;
+const factor = (maxFontSize - minFontSize) / (maxWordSize - minWordSize);
 
 class Card extends Component {
     constructor(props) {
         super(props);
     }
-
     getClassName = () => {
         if (this.props.isMatch)
             return "card disabled match";
@@ -16,9 +22,22 @@ class Card extends Component {
             return "card";
     }
 
+    getSpanStyles = () => {
+        let words = this.props.text.split(' ');
+        let wordsMaxSize = Math.max(words.map(w => w.length));
+        let fontSize = maxFontSize - (factor * wordsMaxSize);
+
+        if (fontSize < minFontSize)
+            fontSize = minFontSize;
+
+        return {
+            fontSize: fontSize + 'px'
+        };
+    }
+
     render() {
         return (
-            <li onClick={() => this.props.toggle(this.props.uid)} className={this.getClassName()}><span>{this.props.text}</span></li>
+            <li onClick={() => this.props.toggle(this.props.uid)} className={this.getClassName()}><span style={this.getSpanStyles()}>{this.props.text}</span></li>
         );
     }
 }
